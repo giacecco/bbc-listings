894f4d5b89d80357a39839685fe4fd56891252cf
Non <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-11-04 18:34:58 +0000

    Added NeDB support

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var async = require('async'),
 	cheerio = require('cheerio'),
+	Nedb = require('nedb'),
 	request = require('request'),
+	argv = require('yargs')
+		.demand([ 'db' ])
+		.default('db', require('path').join(__dirname, 'foo.db'))
+		.argv,
 	_ = require('underscore');
 
 var MEMOISATION_TTL = 5; // minutes
 
+var db = new Nedb({ filename: argv.db, autoload: true });
 
 var getAllProgrammesByLetter_L = function (letter, callback) {
 	letter = letter.toLowerCase();
@@ -12,18 +18,32 @@ var getAllProgrammesByLetter_L = function (letter, callback) {
 		'url': 'http://www.bbc.co.uk/iplayer/a-z/' + letter,
 		'user-agent': 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+',	
 	}, function (error, response, body) {
-		var results = { };
+		var results = [ ];
 	    if (error || response.statusCode != 200) {
 	    	callback(error || new Error('response.statusCode is ' + response.statusCode), results);
 	    } else {
-	    	var $ = cheerio.load(response.body);
+	    	var $ = cheerio.load(response.body),
+	    		record;
 	    	$('#atoz-content ol.tleo-list.left li').each(function (index, element) {
-	    		results[$(element).text().trim().toLowerCase()] = (results[$(element).text().trim().toLowerCase()] || [ ]).concat('http://www.bbc.co.uk' + $('a', element).attr('href'));
+	    		results.push({
+	    			'name': $(element).text().trim(),
+	    			'_id': $('a', element).attr('href').match(/([^/]+)$/)[1],
+	    		});
 	    	});
 	    	$('#atoz-content ol.tleo-list.right li').each(function (index, element) {
-	    		results[$(element).text().trim().toLowerCase()] = (results[$(element).text().trim().toLowerCase()] || [ ]).concat('http://www.bbc.co.uk' + $('a', element).attr('href'));
+	    		results.push({
+	    			'name': $(element).text().trim(),
+	    			'_id': $('a', element).attr('href').match(/([^/]+)$/)[1],
+	    		});
 	    	});
-	    	callback(null, results);
+			async.eachSeries(results, function (result, callback) {
+				db.update(
+					{ '_id': result._id },
+					result,
+					{ 'upsert': true },
+					callback
+				);
+			}, callback);
 	    }
 	});
 };
@@ -45,20 +65,13 @@ var getAllProgrammesByLetter = function (letter, callback) {
 }
 
 var getAllProgrammes = function (callback) {
-	async.reduce([ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	async.eachSeries([ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 		'z', '0-9' ], 
-		{ }, 
-		function (memo, letter, callback) {
-			getAllProgrammesByLetter(letter, function (err, results) {
-				_.extend(memo, memo, results);
-				callback(null, memo);
-			});
-		}, function (err, result) {
-			callback(err, result);
-		});
+		getAllProgrammesByLetter,
+		callback);
 };
 
-getAllProgrammes(function (err, result) {
-	console.log(JSON.stringify(result));
+getAllProgrammes(function (err) {
+	console.log('Done.');
 });
